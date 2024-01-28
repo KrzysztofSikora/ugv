75dc369efd42c61e6d1938535c5a3cb927b3129c
KrzysztofSikora <listdlakrzycha@gmail.com>
2024-01-28 15:39:54 +0100

    Delays solved. Refactor. Ready to live tests.

--- src/movement/movement.module.ts
+++ src/movement/movement.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MovementService } from './movement.service';
 import { MovementController } from './movement.controller';
-import {CommunicationService} from "../communication/communication.service";
-import {AppService} from "../app.service";
 
 @Module({
   controllers: [MovementController],
-  providers: [MovementService, CommunicationService],
+  providers: [MovementService],
   exports: [MovementService],
 
 })
--- src/movement/movement.service.ts
+++ src/movement/movement.service.ts
@@ -1,52 +1,81 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { CreateMovementDto } from './dto/create-movement.dto';
 import { SerialPort } from 'serialport';
-import { ReadlineParser } from "serialport";
-import * as Readline from "readline";
-import {CommunicationService} from "../communication/communication.service";
-import {AppService} from "../app.service";
+import { ReadlineParser } from 'serialport';
 
-// fuser -k /dev/ttyACM0
 @Injectable()
 export class MovementService implements OnModuleInit {
   private movementSerialPort: SerialPort;
-  private readonly movementSerialPortPath: string;
+  private readonly movementSerialPortPath: string = '/dev/ttyUSB0';
 
-  constructor(private readonly communicationService: CommunicationService) {
-    this.movementSerialPortPath = '/dev/ttyACM1';
+  private communicationSerialPort: SerialPort;
+  private readonly communicationSerialPortPath: string = '/dev/ttyACM0';
+  private parser: ReadlineParser;
+
+  /**
+   * Init serials port. If any port has busy.
+   * Use fuser -k /dev/<port> for kill.
+   * For example: fuser -k /dev/ttyACM0
+   */
+  async onModuleInit() {
+    this.startMovement();
+    this.startRemoteCommunication();
+  }
+
+  /**
+   * Receiver messages from 868Mhz communication.
+   */
+  startRemoteCommunication() {
+    this.communicationSerialPort = new SerialPort({
+      path: this.communicationSerialPortPath,
+      baudRate: 115200,
+    });
+    this.parser = this.communicationSerialPort.pipe(
+      new ReadlineParser({ delimiter: '\n' }),
+    );
+
+    this.communicationSerialPort.on('open', () => {
+      console.log(
+        'Communication service. Listening for incoming messages from 868Mhz',
+      );
+    });
+
+    this.parser.on('data', (data) => {
+      /* Sending movement direction message received from 868Mhz */
+      console.log('Received message:', data);
+      if (data) this.sendMessage(data);
+    });
+  }
+
+  /**
+   * Start serial communication with movement controller.
+   */
+  startMovement() {
     this.movementSerialPort = new SerialPort({
       path: this.movementSerialPortPath,
       baudRate: 115200,
-      lock: false,
     });
 
-
-  }
-  async onModuleInit() {
-    this.startSerialPortConnection();
-    this.communicationService.startCommunication(this.sendMoveMessage);
-
-  }
-  startSerialPortConnection() {
     this.movementSerialPort.on('open', () => {
       console.log('Movement service. Waiting for send direction message');
     });
   }
-  sendMoveMessage(message: string) {
-    this.movementSerialPort.write(
-        `${message}\r\n`,
-    );
-    console.log("Send message: ",message)
-  }
+
+  /**
+   * Send message from HTTP method.
+   * @param createMovementDto
+   */
   createMove(createMovementDto: CreateMovementDto) {
+    this.sendMessage(createMovementDto.direction[0]);
+    return { action: `Move to direction: ${createMovementDto.direction}` };
+  }
 
-
-    // send data
-    this.communicationService.sendMessage(createMovementDto.direction)
-
-    // this.movementSerialPort.write(
-    //   `${createMovementDto.direction}\r\n`,
-    // );
-    return `Move to dirdsection: ${createMovementDto.direction}`;
+  /**
+   * Send message by serial port to movement controller.
+   * @param message
+   */
+  sendMessage(message: string) {
+    console.log(`Send message: ${message}`);
+    this.movementSerialPort.write(`${message}\n`);
   }
 }
